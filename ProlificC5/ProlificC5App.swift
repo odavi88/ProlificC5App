80b3f3c198fcaf0ae30d5d4eb44b7e4e1f7f145f
Omar D <omardavidson227@gmail.com>
2023-03-19 22:08:59 -0400

    Initial Commit

---
+++ ProlificC5/ProlificC5App.swift
@@ -0,0 +1,17 @@
+//
+//  ProlificC5App.swift
+//  ProlificC5
+//
+//  Created by Omar Davidson II on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ProlificC5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
